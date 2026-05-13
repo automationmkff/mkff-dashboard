d9bed8cea7affbb4988da0db1e3abe36c79b51d7
automationmkff <automationmkff@gmail.com>
2026-05-13 19:24:07 +0700

    Update sheets.js

--- netlify/functions/sheets.js
+++ netlify/functions/sheets.js
@@ -1,28 +1,52 @@
-const https = require('https');
+var https = require('https');
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyyhuuN7RvUGOIo1OdMLlY5tJsHfH7Ikx7i_cwaQDI_3G6njuSTpOvrbUYN659c1-1I/exec';
+var APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyyhuuN7RvUGOIo1OdMLlY5tJsHfH7Ikx7i_cwaQDI_3G6njuSTpOvrbUYN659c1-1I/exec';
+var FX_URL = 'https://api.frankfurter.app/latest?from=USD&to=THB';
 
-function httpsGet(url, redirectCount = 0) {
-  return new Promise((resolve, reject) => {
+function httpsGet(url, redirectCount) {
+  redirectCount = redirectCount || 0;
+  return new Promise(function(resolve, reject) {
     if (redirectCount > 10) return reject(new Error('Too many redirects'));
-    https.get(url, { headers: { 'Accept': 'application/json' } }, (res) => {
+    https.get(url, { headers: { 'Accept': 'application/json' } }, function(res) {
       if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
         return resolve(httpsGet(res.headers.location, redirectCount + 1));
       }
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => resolve({ status: res.statusCode, body: data }));
+      var data = '';
+      res.on('data', function(chunk) { data += chunk; });
+      res.on('end', function() { resolve({ status: res.statusCode, body: data }); });
     }).on('error', reject);
   });
 }
 
-exports.handler = async function(event, context) {
-  try {
-    const result = await httpsGet(APPS_SCRIPT_URL);
+exports.handler = function(event, context) {
+  var params = event.queryStringParameters || {};
+
+  // FX rate endpoint
+  if (params.type === 'fx') {
+    return httpsGet(FX_URL).then(function(result) {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+        body: result.body
+      };
+    }).catch(function(error) {
+      return {
+        statusCode: 500,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify({ error: error.message })
+      };
+    });
+  }
+
+  // Google Sheets endpoint (default)
+  return httpsGet(APPS_SCRIPT_URL).then(function(result) {
     if (result.status !== 200) {
-      throw new Error(`Apps Script returned ${result.status}: ${result.body}`);
+      throw new Error('Apps Script returned ' + result.status + ': ' + result.body);
     }
-    const data = JSON.parse(result.body);
+    var data = JSON.parse(result.body);
     return {
       statusCode: 200,
       headers: {
@@ -32,11 +56,11 @@ exports.handler = async function(event, context) {
       },
       body: JSON.stringify(data)
     };
-  } catch (error) {
+  }).catch(function(error) {
     return {
       statusCode: 500,
       headers: { 'Access-Control-Allow-Origin': '*' },
       body: JSON.stringify({ error: error.message })
     };
-  }
+  });
 };
